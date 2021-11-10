a9cbb58457c2aff12a5ebb05b90aedf4e7fb91d5
opilane <opilane@HP-CZC6297B92>
2021-11-10 13:59:28 +0200

    Add project files.

---
+++ Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    class Triangle
+    {
+        public double a;//первая сторона
+        public double b;//вторая сторона
+        public double c;//третья сторона
+    }
+    public Triangle(double A, double B, double C)//конструктор
+    {
+        a = A;//создаем с заданными длинами сторон соглсано заданию
+        b = B;
+        c = C;
+    }
+    public string outputA()//выводим сторону a, данный метод возвращает строковое значение
+
+}
